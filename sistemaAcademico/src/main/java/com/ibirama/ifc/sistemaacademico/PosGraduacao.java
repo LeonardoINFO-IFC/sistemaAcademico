99c249e5e1a70b66ada7b672d3ed01456ad05e40
Iago Vinicius Schmitz <iago.schmitz007@gmail.com>
2026-04-01 12:00:21 -0300

    pronto finalite

---
+++ sistemaAcademico/src/main/java/com/ibirama/ifc/sistemaacademico/PosGraduacao.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ibirama.ifc.sistemaacademico;
+
+/**
+ *
+ * @author aluno
+ */
+public class PosGraduacao extends Curso{
+    private String tipo;
+    private String areaPesquisa;
+    
+    public String getTipo(){
+        return tipo;
+  }
+    
+    public void setTipo(String _tipo){
+        tipo = _tipo;
+    }
+    
+    public String getAreaPesquisa(){
+        return areaPesquisa;
+    }
+    
+    public void setAreaPesquisa(String _areaPesquisa){
+        areaPesquisa = _areaPesquisa;
+    }
+    
+    public void validarDissertacaoOuTese(){
+            
+    }
+}
